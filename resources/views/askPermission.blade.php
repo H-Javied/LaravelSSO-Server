56147b46889c0a3eb024f0d75a516d82841de3cc
Hamza Javied <hamzajavied@invotyx.co.uk>
2022-11-01 16:24:48 +0500

    permission to use new feature

---
+++ resources/views/askPermission.blade.php
@@ -0,0 +1,6 @@
+<h1>Permission Required</h1>
+
+<h5>you are not permitted to view this page</h5>
+<h3>Ask for permission</h3><a class="btn btn-link" href="{{ route('permission.request') }}">
+    {{ __('Forgot Your Password?') }}
+</a>
