746ca10c3352a34d1fcd922a555dedb71f9aaa09
Hamza Javied <hamzajavied@invotyx.co.uk>
2022-11-02 12:52:53 +0500

    link to blog-Posts added

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,11 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/permission-request', function () {
+    return view('askPermission')->with(['user' => 'all']);
+})->name('permission.request');
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/blog', [App\Http\Controllers\HomeController::class, 'blogPosts'])->name('blogs');
 Route::get('/different-account', [App\Http\Controllers\HomeController::class, 'getDifferentAccount'])->name('different-account');
