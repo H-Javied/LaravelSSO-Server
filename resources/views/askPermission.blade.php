a7af232333aed38f6ea9cf0e71d925f502a7ee37
Hamza Javied <hamzajavied@invotyx.co.uk>
2022-11-02 12:55:55 +0500

    permission link set

--- resources/views/askPermission.blade.php
+++ resources/views/askPermission.blade.php
@@ -2,5 +2,5 @@
 
 <h5>you are not permitted to view this page</h5>
 <h3>Ask for permission</h3><a class="btn btn-link" href="{{ route('permission.request') }}">
-    {{ __('Forgot Your Password?') }}
+    {{ __('HERE') }}
 </a>
